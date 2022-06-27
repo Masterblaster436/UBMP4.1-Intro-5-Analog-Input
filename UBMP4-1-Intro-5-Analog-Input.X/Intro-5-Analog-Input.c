162d1b5d34ed2b68e3485efa355892fb9370a8d1
Ryan Massey <99211959+Masterblaster436@users.noreply.github.com>
2022-06-27 00:07:09 -0400

    Update Intro-5-Analog-Input.c

--- UBMP4-1-Intro-5-Analog-Input.X/Intro-5-Analog-Input.c
+++ UBMP4-1-Intro-5-Analog-Input.X/Intro-5-Analog-Input.c
@@ -5,8 +5,16 @@
 #include    "UBMP410.h"         // Include UBMP4.1 constant and function definitions
 #include    "Simple-Serial.h"   // Include simple serial debug function definitions
 #include    "lcd.h"             // LCD Functions
+#include    "time.h"            // Include time 
 
 #define _XTAL_FREQ 8000000
+
+// void Time()
+// {
+//     time_t t;   // not a primitive datatype
+//     time(&t);
+//     printf("\n current time is : %s",ctime(&t));
+// }
 
 int main()
 {
